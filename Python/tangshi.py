a217ca2f3c381c72bbed280a00113a361f22113b
James Worsley <jmworsley@gmail.com>
2014-07-28 22:40:51 +1000

    Added Ping, Shang, Qu and Ru tags

--- Python/tangshi.py
+++ Python/tangshi.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python
-
 # -*- coding: utf-8 -*-
 
 import sys
+import re
+import codecs
+
+ping = re.compile(u'.平')
+shang = re.compile(u'上聲')
+ru = re.compile(u'入')
+qu = re.compile(u'去')
 
 mydict = { }
 
-f = open("../Data/TangRhymesMap.csv")
+# f = open("../Data/TangRhymesMap.csv")
+f = codecs.open("../Data/TangRhymesMap.csv", "r", "utf-8")
 
 for line in f:
   line = line.rstrip()
-  rhyme, char = line.split(",")
-  #key = str(char)
-  #value = str(rhyme)
-  key = char.decode("utf-8")
-  value = rhyme.decode("utf-8")
+  value, key = line.split(",")
+  #key = char.decode("utf-8")
+  #value = rhyme.decode("utf-8")
   mydict[key] = value
 
-f = open("../Data/SamplePoem.txt")
+f = codecs.open("../Data/SamplePoem.txt", "r", "utf-8")
 for line in f:
   line = line.rstrip()
-  for key in line.decode("utf-8"):
-    print key + " = " + mydict[key] + '\n'
+  for key in line:
+    if ping.match(mydict[key]):
+      print key + " = " + " Ping"
+    elif shang.match(mydict[key]):
+      print key + " = " + " Shang"
+    elif qu.match(mydict[key]):
+      print key + " = " + " Qu"
+    elif ru.match(mydict[key]):
+      print key + " = " + " Ru"
+    else:
+      print key + " = " + " *"
 
